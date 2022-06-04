206b6b3df62fd0a1d72c5aa03678e69408d68e79
Vika Tsykunova <vika.tsykunova02@gmail.com>
2022-06-04 22:43:44 +0300

    Make all home

--- countries/script.js
+++ countries/script.js
@@ -1,30 +1,30 @@
-var countries = { Georgia:'Tbilisi', Kazakhstan:'Nur-Sultan', Poland:'Warsaw' };
+var Allcountries = { Georgia:'Tbilisi', Kazakhstan:'Nur-Sultan', Poland:'Warsaw' };
 
         
        function add() {
             var country = prompt ('Введите название страны');
             var capital = prompt ('Введите название ее столицы');
-            countries[country] = capital;
+            Allcountries[country] = capital;
             console.log (countries);
         }
 
         function info() {
             var country = prompt ('Введите название страны');
-            if (country in countries) {
-                alert (countries[country])
+            if (country in Allcountries) {
+                alert (Allcountries[country])
             } else {
                 alert ('нет информации о стране')
             }
         }
 
         function Countries () {
-            for (var k in countries)
-            console.log (k + ": " + countries[k])
+            for (var k in Allcountries)
+            console.log (k + ": " + Allcountries[k])
         }
 
         function trash() {
             var country = prompt ('Введите название страны');
-            delete countries[country];
-            console.log (countries);
+            delete Allcountries[country];
+            console.log (Allcountries);
             }
 
